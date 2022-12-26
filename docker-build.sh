81ae43523c22b5f6e67241856426c3de97bea11b
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-12-26 17:23:11 +0530

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,5 @@
 git clone https://github.com/ranvirkardhanraj/clumsy-bird.git
-sleep 10
-cd clumsy-bird/src
-docker buld -t dhanrajranvirkar/hextris:v1
-sleep 10
-cd
+docker build /home/minikube-admin/clumsy-bird/ -t dhanrajranvirkar/hextris:$BUILD_NUMBER
 rm -rvf clumsy-bird
 docker push dhanrajranvirkar/hextris:$BUILD_NUMBER
-sleep 10
 docker rmi dhanrajranvirkar/hextris:$BUILD_NUMBER
