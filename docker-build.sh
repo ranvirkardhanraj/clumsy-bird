7fe4faf4cd9557068d4a7f9df8515880b1f01d31
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-12-27 00:16:19 +0530

    Delete docker-build.sh

--- docker-build.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/ranvirkardhanraj/clumsy-bird.git
-docker build /home/minikube-admin/clumsy-bird/ -t dhanrajranvirkar/hextris:$BUILD_NUMBER
-rm -rvf clumsy-bird
-docker push dhanrajranvirkar/hextris:$BUILD_NUMBER
-docker rmi dhanrajranvirkar/hextris:$BUILD_NUMBER
