0a78e8b27acc7d6e9354bb567c9dfaf38bce44ed
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-12-26 16:14:41 +0530

    Create docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/ranvirkardhanraj/clumsy-bird.git
+sleep 10
+cd clumsy-bird/src
+docker buld -t dhanrajranvirkar/hextris:$BUILD_NUMBER
+sleep 10
+cd
+rm -rvf clumsy-bird
+docker push dhanrajranvirkar/hextris:$BUILD_NUMBER
+sleep 10
+docker rmi dhanrajranvirkar/hextris:$BUILD_NUMBER
